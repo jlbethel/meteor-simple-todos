848cf52cae79007bac2f036d8b412106c9889d5c
Jason Bethel <jlbethel@gmail.com>
2016-02-28 16:02:34 -0800

    add code from meteor tutorial through step 5

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,42 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   //This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"}
-    ]
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   });
+
+Template.body.events({
+  "submit .new-task": function (event) {
+    //Prevent default browser form submit
+    event.preventDefault();
+
+    //Get Value from form element
+    var text = event.target.text.value;
+    console.log(event);
+
+    //Insert a task into the Collection
+    Tasks.insert({
+      text:text,
+      createdAt: new Date() //current time
+    });
+    //Clear form
+    event.target.text.value = "";
+  }
+});
+
+Template.task.events({
+
+  "click .toggle-checked": function() {
+    //Set the checked property to the opposite of it current value
+    Tasks.update(this._id, {
+      $set: {checked: ! this.checked}
+    });
+  },
+  "click .delete": function () {
+    Tasks.remove(this._id);
+  }
+});
 }
